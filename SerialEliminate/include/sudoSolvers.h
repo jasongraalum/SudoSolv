c16e9f01fc0f653de5846d8257336f692406c8c5
cpi <jasongraalum@gmail.com>
2019-03-04 02:49:49 +0000

    Clean up

---
+++ SerialEliminate/include/sudoSolvers.h
@@ -0,0 +1,28 @@
+// // Copyright (c) 2019 Jason Graalum,
+//
+// CS 531 Performance Analysis
+// Portland State University
+// Winter 2019
+//
+// Jason Graalum
+// Feb 2, 2019
+//
+// sudoSolv Data structure
+//
+// Puzzle Data Structure
+
+#ifndef _SUDOSolver_H_
+#define _SUDOSolver_H_
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "sudoSolvUtils.h"
+
+int elimSolve(Puzzle *);
+int addGuess(Puzzle *, int, int);
+
+#endif
+
+
