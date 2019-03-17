bb9712dd8a5d3c2d77c7c1067cbfc5d40528d050
cpi <jasongraalum@gmail.com>
2019-03-16 21:40:12 -0700

    Final updates to programs. Clean up.

--- MPIBacktrack/run_test_16no1.sh
+++ MPIBacktrack/run_test_16no1.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 sn=`hostname`
+
 make run16no1 >~/$sn.mpibt_16no1_try1.out
+make run16x3 >~/$sn.mpibt_16x3_try1.out
+make run16x4 >~/$sn.mpibt_16x4_try1.out
+
+for i in {1..99}
+do
 make run16no1 >>~/$sn.mpibt_16no1_try1.out
-make run16no1 >>~/$sn.mpibt_16no1_try1.out
-make run16no1 >>~/$sn.mpibt_16no1_try1.out
-make run16no1 >>~/$sn.mpibt_16no1_try1.out
-make run16no1 >>~/$sn.mpibt_16no1_try1.out
-make run16no1 >>~/$sn.mpibt_16no1_try1.out
-make run16no1 >>~/$sn.mpibt_16no1_try1.out
-make run16no1 >>~/$sn.mpibt_16no1_try1.out
-make run16no1 >>~/$sn.mpibt_16no1_try1.out
+make run16x3 >>~/$sn.mpibt_16x3_try1.out
+make run16x4 >>~/$sn.mpibt_16x4_try1.out
+done
